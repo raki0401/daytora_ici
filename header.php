3ddaab1bd3b351db052a020f8332536ba718f4d2
牧大介 <raki0401@outlook.jp>
2021-06-30 22:26:45 +0900

    テキスト変更

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
         )
       );
       ?>
-      <!-- <nav class="header-nav">--
+      <!-- <nav class="header-nav">-----
         <ul class="header-list">
           <li><a href="#top" class="header-nav-top little">TOP</a></li>
           <li><a href="news.html" class="header-nav-news">NEWS</a></li>
